901952182c89b265883e605c7e0f67543a586865
Aleksandr <93007451+sxkxixx@users.noreply.github.com>
2022-09-29 21:53:57 +0500

    Отрефакторил код

--- js/main.js
+++ js/main.js
@@ -1,3 +1,6 @@
+const photosAmount = 25;
+const likesAmount = {min: 15, max: 200};
+
 const MESSAGES = ['Всё отлично!', 'В целом всё неплохо. Но не всё.',
   'Когда вы делаете фотографию, хорошо бы убирать палец из кадра. В конце концов это просто непрофессионально.',
   'Моя бабушка случайно чихнула с фотоаппаратом в руках и у неё получилась фотография лучше.',
@@ -19,8 +22,13 @@ const getRandomNum = (a, b) => {
   return Math.floor(Math.random() * (upper - lower + 1) + lower);
 };
 
+
 const isRightString = (str, maxLen) => String(str).length <= maxLen;
+
+
 isRightString('Линтер не любит, когда функция не вызвана', 50);
+
+
 const getRandomElement = (array) => array[getRandomNum(0, array.length - 1)];
 
 
@@ -39,9 +47,10 @@ function createDescription(id){
     id: id,
     url: `photos/${id}.jpg`,
     description: getRandomElement(DESCRIPTION),
-    likes: getRandomNum(15, 200),
+    likes: getRandomNum(likesAmount.min, likesAmount.max),
     comments: Array.from({length: getRandomNum(1, 6)}).map((value, index) => createCommentObj(index + 1))
   };
 }
+
 // eslint-disable-next-line no-unused-vars
-const arrayObj = Array.from({length: 25}).map((value, index) => createDescription(index + 1));
+const arrayObj = Array.from({length: photosAmount}).map((value, index) => createDescription(index + 1));
