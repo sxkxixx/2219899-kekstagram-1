98a9078a48159a79dc900adee9a376bbf53a450a
Aleksandr <93007451+sxkxixx@users.noreply.github.com>
2022-10-10 10:27:35 +0500

    Внес правки

--- js/main.js
+++ js/main.js
@@ -1,5 +1,5 @@
 import {createDescription, photosAmount} from './create-data.js';
 
+// eslint-disable-next-line no-unused-vars
 const arrayObj = Array.from({length: photosAmount}).map((value, index) => createDescription(index + 1));
-// eslint-disable-next-line no-console
-console.log(arrayObj);
+
